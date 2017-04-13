7330ffc968e0d2778175dca955cc966170e8e742
LonelyEnvoy <petrinchor@gmail.com>
2017-04-13 23:59:21 +0800

    added solution for POJ 3481

---
+++ src/POJ/POJ-3481.cpp
@@ -0,0 +1,38 @@
+/**
+ * Created by LonelyEnvoy on 2017-4-13.
+ * Double Queue
+ * Keywords:
+ */
+
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <limits>
+using namespace std;
+
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define erep(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n-1; i >= a; i--)
+
+const int INF = 0x3f3f3f3f;
+const double EPS = numeric_limits<double>::epsilon();
+typedef long long ll;
+// end of header
+
+int main() {
+
+}
