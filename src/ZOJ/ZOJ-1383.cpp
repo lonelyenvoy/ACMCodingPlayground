f4ca979cb6a0d3329988e5c936e2361a33073cbd
envoy <petrinchor@gmail.com>
2017-06-18 22:56:33 +0800

    added solution for ZOJ 1383

---
+++ src/ZOJ/ZOJ-1383.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+#define rep(i,a,n) for (int i = a; i < n; ++i)
+#define erep(i,a,n) for (int i = a; i <= n; ++i)
+#define per(i,a,n) for (int i = n-1; i >= a; --i)
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int oct;
+    const int bound = sizeof(int)*8;
+    bool first;
+    while (n--) {
+        first = true;
+        scanf("%d", &oct);
+        rep(i,0,bound) {
+            if ((1 << i) & oct) {
+                if (first) {
+                    first = false;
+                    printf("%d", i);
+                } else {
+                    printf(" %d", i);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
