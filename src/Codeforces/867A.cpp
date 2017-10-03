39dde38b5ca70ed4ac442a5224e0978a8a689ee6
lonelyenvoy <petrinchor@gmail.com>
2017-10-03 18:12:38 +0800

    add solution for Codeforces 867A

---
+++ src/Codeforces/867A.cpp
@@ -0,0 +1,36 @@
+/**
+ * Created by LonelyEnvoy on 2017-10-3.
+ * A. Between the Offices
+ * Keywords: simple strings
+ */
+
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+#define rep(i,a,n) for(int i=a;i<n;++i)
+#define erep(i,a,n) for(int i=a;i<=n;++i)
+#define per(i,a,n) for(int i=n-1;i>=a;--i)
+
+#define MAX 101
+
+int n;
+char last;
+char curr;
+int s;
+int f;
+
+int main() {
+    scanf("%d", &n);
+    getchar();
+    rep(i,0,n) {
+        last = curr;
+        scanf("%c", &curr);
+        if (last == 'S' && curr == 'F') s++;
+        else if (last == 'F' && curr == 'S') f++;
+    }
+    printf(s > f ? "YES\n" : "NO\n");
+
+}
+
+
