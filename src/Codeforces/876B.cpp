6e6fb72ad4ac662e97fdf8fe9fb9f475eb448461
lonelyenvoy <lonelyenvoy@gmail.com>
2017-10-19 21:33:01 +0800

    add solution for Codeforces 876B

---
+++ src/Codeforces/876B.cpp
@@ -0,0 +1,44 @@
+/**
+ * Created by LonelyEnvoy on 2017-10-19.
+ * B. Divisiblity of Differences
+ * Keywords: math
+ */
+
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+#define rep(i,a,n) for(int i=a;i<n;++i)
+#define erep(i,a,n) for(int i=a;i<=n;++i)
+#define per(i,a,n) for(int i=n-1;i>=a;--i)
+
+#define MAX 100001
+
+int n, k, m;
+int a[MAX];
+int mod[MAX];
+int c[MAX];
+int peak, best;
+
+int main() {
+    scanf("%d%d%d", &n, &k, &m);
+    rep(i,0,n) scanf("%d", a+i), mod[i] = a[i] % m, c[mod[i]]++;
+    rep(i,0,m) if (c[i] > best) peak = i, best = c[i];
+    if (best < k) return puts("No"), 0;
+    puts("Yes");
+    int count = 0;
+    bool first = true;
+    rep(i,0,n) {
+        if (count >= k) break;
+        if (mod[i] == peak) {
+            if (!first) printf(" ");
+            printf("%d", a[i]);
+            first = false;
+            count++;
+        }
+    }
+    printf("\n");
+}
+
+
